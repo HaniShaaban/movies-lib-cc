c2aab6919a46193126e52f6918be1c4e5ea0271a
HaniShaaban <hani.w.shaaban@gmail.com>
2025-10-25 14:04:18 +0300

    chore: setup a docker compose and added DB service

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # movies-lib-cc
+```
+movies-lib-cc
+├─ .DS_Store
+├─ README.md
+├─ be
+│  ├─ .eslintrc.js
+│  ├─ .prettierrc
+│  ├─ README.md
+│  ├─ nest-cli.json
+│  ├─ package-lock.json
+│  ├─ package.json
+│  ├─ src
+│  │  ├─ app.controller.ts
+│  │  ├─ app.module.ts
+│  │  ├─ app.service.ts
+│  │  └─ main.ts
+│  ├─ tsconfig.build.json
+│  └─ tsconfig.json
+└─ docker-compose.yml
+```
---
+++ be/prisma/migrations/20251025110219_add_movie_director_genre/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "Director" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Director_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Genre" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "synopsis" TEXT,
+    "year" INTEGER NOT NULL,
+    "cover" TEXT,
+    "directorId" INTEGER NOT NULL,
+    "genreId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Genre_name_key" ON "Genre"("name");
+
+-- AddForeignKey
+ALTER TABLE "Movie" ADD CONSTRAINT "Movie_directorId_fkey" FOREIGN KEY ("directorId") REFERENCES "Director"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Movie" ADD CONSTRAINT "Movie_genreId_fkey" FOREIGN KEY ("genreId") REFERENCES "Genre"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
